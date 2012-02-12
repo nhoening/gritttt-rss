9ea462011402b9aeb0b917c6c8fecbafe6754f7a
Nicolas Honing <nhoening@gmail.com>
2012-02-12 22:37:27 +0100

    make sharein-widget an overlay, improve form

--- shareintt-widget/dialog.js
+++ shareintt-widget/dialog.js
@@ -1,36 +1,35 @@
 
 (function(){
 
-  var iframe_url = "http://www.nicolashoening.de/tt-unifeed/form.html" 
-  var iframe_id = 'tt-unifeed-iframe';
+    var iframe_url = "http://www.nicolashoening.de/gritttt-rss/form.html" 
+    var iframe_id = 'gritttt-iframe';
+    var div_id = 'gritttt-div';
 
-  // TODO: check if div is there already, then just refill title and url 
-  //var existing_iframe = document.getElementById(iframe_id);
+    if (document.getElementById(div_id) == null) {
+        // TODO:  CSS to style the box, use right:0 ?
+        var headID = document.getElementsByTagName("head")[0];
+        var cssNode = document.createElement('link');
+        cssNode.type = 'text/css';
+        cssNode.rel = 'stylesheet';
+        cssNode.href = url;
+        cssNode.media = 'screen';
+        headID.appendChild(cssNode);
 
-  // TODO:  CSS to style the box, use right:0 ?
-  var headID = document.getElementsByTagName("head")[0];
-  var cssNode = document.createElement('link');
-  cssNode.type = 'text/css';
-  cssNode.rel = 'stylesheet';
-  cssNode.href = url;
-  cssNode.media = 'screen';
-  headID.appendChild(cssNode);
+        // display forn in iframe
+        var div = document.createElement("div");
+        div.id = div_id;
+        var str = "<div onclick='document.getElementById(\"" + div_id + "\").style.display=\"none\";' style='position:fixed;top:0px;width:100%;height:100%;z-index:10000;background: #C3e2fd;' title='click to close window' valign='top' align='center' width='20px'>";
+        str += "<iframe frameborder='0' scrolling='no' name='" + iframe_id + "' id='" + iframe_id + "' src='" + iframe_url + "' width='550px' height='75px' style='textalign:right; backgroundColor: white;'></iframe>";
+        str += "</div>"
+        div.innerHTML = str;
+        document.body.insertBefore(div, document.body.firstChild);
+    } else {
+        document.getElementById(div_id).style.display = 'block';
+    }
 
-  // display forn in iframe
-  var div = document.createElement("div");
-  div.id = "tt-unifeed-bookmarklet";
-  var str = "";
-  str += "<table id='instacalc_bookmarklet_table' valign='top' width='570' cellspacing='0' cellpadding='0'><tr><td width ='550' height='80'>";
-  str += "<iframe frameborder='0' scrolling='no' name='" + iframe_id + "' id='" + iframe_id + "' src='" + iframe_url + "' width='550px' height='75px' style='textalign:right; backgroundColor: white;'></iframe>";
-  str += "</td><td onclick='document.getElementById(\"" + div.id + "\").style.display=\"none\";' style='background: #C3e2fd;' title='click to close window' valign='top' align='center' width='20px'>";
-  str += "<a href='javascript:void(0);' style='width:100%; text-align: middle; color: #FF0000; font-family: Arial;'>x</a>";
-  str += "</td></tr></table>";
-  div.innerHTML = str;
-  document.body.insertBefore(div, document.body.firstChild);
-
-  // TODO: set title and url
-  var title = document.title;
-  var url = location.href;
+    // TODO: set title and url
+    var title = document.title;
+    var url = location.href;
   
 })()
 
