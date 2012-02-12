881f7f9917bb040ff9fa5bfbee3d362e1ffad34b
nhoening <nhoening@gmail.com>
2012-02-13 00:10:26 +0100

    populate form, remove some unused styles

--- shareintt-widget/dialog.js
+++ shareintt-widget/dialog.js
@@ -1,25 +1,26 @@
 
 (function(){
 
-    var iframe_url = "http://www.nicolashoening.de/gritttt-rss/form.html" 
+    var iframe_url = "http://www.nicolashoening.de/gritttt-rss/form.html";
+    iframe_url += "?url=" + encodeURIComponent(location.href);
+    iframe_url += "&title=" + encodeURIComponent(document.title); 
     var iframe_id = 'gritttt-iframe';
     var div_id = 'gritttt-div';
 
     if (document.getElementById(div_id) == null) {
-        // TODO:  CSS to style the box, use right:0 ?
-        var headID = document.getElementsByTagName("head")[0];
-        var cssNode = document.createElement('link');
-        cssNode.type = 'text/css';
-        cssNode.rel = 'stylesheet';
-        cssNode.href = url;
-        cssNode.media = 'screen';
-        headID.appendChild(cssNode);
+        var head = document.getElementsByTagName("head")[0];
+        var css = document.createElement('link');
+        css.type = 'text/css';
+        css.rel = 'stylesheet';
+        css.href = ""; //TODO
+        css.media = 'screen';
+        head.appendChild(css);
 
         // display forn in iframe
         var div = document.createElement("div");
         div.id = div_id;
-        var str = "<div onclick='document.getElementById(\"" + div_id + "\").style.display=\"none\";' style='position:fixed;top:0px;width:100%;height:100%;z-index:10000;background: #C3e2fd;' title='click to close window' valign='top' align='center' width='20px'>";
-        str += "<iframe frameborder='0' scrolling='no' name='" + iframe_id + "' id='" + iframe_id + "' src='" + iframe_url + "' width='550px' height='75px' style='textalign:right; backgroundColor: white;'></iframe>";
+        var str = "<div onclick='document.getElementById(\"" + div_id + "\").style.display=\"none\";' style='position:fixed;top:0px;width:100%;height:100%;z-index:10000;background: #C3e2fd;' title='click to close window'>";
+        str += "<iframe frameborder='0' scrolling='no' name='" + iframe_id + "' id='" + iframe_id + "' src='" + iframe_url + "' width='550px' height='275px'></iframe>";
         str += "</div>"
         div.innerHTML = str;
         document.body.insertBefore(div, document.body.firstChild);
@@ -27,9 +28,6 @@
         document.getElementById(div_id).style.display = 'block';
     }
 
-    // TODO: set title and url
-    var title = document.title;
-    var url = location.href;
   
 })()
 
--- shareintt-widget/share.php
+++ shareintt-widget/share.php
@@ -3,13 +3,15 @@
  This script takes website details from the form and inserts
  them into the tt-rss DB.
 
- It needs to know database credentials, id of user in DB and unifeed-id
+ It needs to know database credentials, id of user in DB and gritttt-id
  In particular,
  * Make new entry in ttrss_entries, set (title, link, content) from request, 
    also set updated, published & read. Remember new-id
- * Make new entry in ttrss_user_entries, set (new-id->ref_id, unifeed-id->feed_id, user-id->owner_uid) 
+ * Make new entry in ttrss_user_entries, set (new-id->ref_id, gritttt-id->feed_id, user-id->owner_uid) 
  
  TODO: check functions.php in tt-rss for usable code
  */
 
+
+return 1;
 ?>
