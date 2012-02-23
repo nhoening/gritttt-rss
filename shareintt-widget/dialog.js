34bec21ea2874b103d946897ae3e6b6a63a33063
nhoening <nhoening@gmail.com>
2012-02-23 17:46:07 +0100

    style share widget, protect against quotes in page title, simplify inserted HTML

--- shareintt-widget/dialog.js
+++ shareintt-widget/dialog.js
@@ -4,26 +4,28 @@
     var host = 'http://www.nicolashoening.de';
     var iframe_url = host + "/gritttt-rss/form.html";
     iframe_url += "?url=" + encodeURIComponent(location.href);
-    iframe_url += "&title=" + encodeURIComponent(document.title); 
+    iframe_url += "&title=" + encodeURIComponent(document.title);
     var iframe_id = 'gritttt-iframe';
     var div_id = 'gritttt-div';
+    var css_url = host + "/gritttt-rss/dialog.css";
 
     if (document.getElementById(div_id) == null) {
         var head = document.getElementsByTagName("head")[0];
         var css = document.createElement('link');
         css.type = 'text/css';
         css.rel = 'stylesheet';
-        css.href = ""; //TODO
+        css.href = css_url;
         css.media = 'screen';
         head.appendChild(css);
 
-        // display forn in iframe
+        // display form in iframe
         var div = document.createElement("div");
         div.id = div_id;
-        var str = "<div onclick='document.getElementById(\"" + div_id + "\").style.display=\"none\";' style='position:fixed;top:0px;width:100%;height:100%;z-index:10000;background: #C3e2fd;' title='click to close window'>";
-        str += "<iframe frameborder='0' scrolling='no' name='" + iframe_id + "' id='" + iframe_id + "' src='" + iframe_url + "' width='550px' height='275px'></iframe>";
-        str += "</div>"
-        div.innerHTML = str;
+        div.title = "click to close window.";
+        div.onclick = function(){
+            document.getElementById(div_id).style.display='none';
+        };
+        div.innerHTML = '<iframe frameborder="0" scrolling="no" name="' + iframe_id + '" id="' + iframe_id + '" src="' + iframe_url.replace('"', "'") + '" width="600px" height="200px"></iframe>';
         document.body.insertBefore(div, document.body.firstChild);
    
         // listen for success/failure report 
@@ -37,13 +39,14 @@
                 console.log('host confirmed');
                 if (e.data == 'shared') {
                     document.getElementById(div_id).style.display='none';
+                    // TODO: refill iframe or nullify div, or make sense in another way of 
+                    //       a follow-up click on bookmarklet
                 }
                 else if(e.data == 'failure') {
                     alert('oops');
                 }
             }
         },false);
-
 
     } else {
         document.getElementById(div_id).style.display = 'block';
