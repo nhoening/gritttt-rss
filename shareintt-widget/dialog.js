1c84d29eab7c6be81492c114a29f047276cd5c62
Nicolas Honing <nhoening@gmail.com>
2012-03-07 01:15:09 +0100

    sharein - widget: roundtrip to check wether the user is logged in to tt-rss; refuse to go on otherwise. Some styling and workflow in dialog.js

--- shareintt-widget/dialog.js
+++ shareintt-widget/dialog.js
@@ -1,34 +1,50 @@
+// Adapt this
+var host = 'http://www.nicolashoening.de';
+var ttrss_host = 'http://tt-rss.nicolashoening.de';
+var path_to_gritttt = '/gritttt-rss';
 
-(function(){
+var overlay_id = 'gritttt-overlay';
+var box_id = 'gritttt-box';
+var msg_id = 'gritttt-msg';
 
-    var host = 'http://www.nicolashoening.de';
-    var iframe_url = host + "/gritttt-rss/form.html";
+
+function show_share_form(overlay)
+{
+    var iframe_url = host + path_to_gritttt + "/form.html";
     iframe_url += "?url=" + encodeURIComponent(location.href);
     iframe_url += "&title=" + encodeURIComponent(document.title);
-    var iframe_id = 'gritttt-iframe';
-    var div_id = 'gritttt-div';
-    var css_url = host + "/gritttt-rss/dialog.css";
+    overlay.innerHTML = '<iframe frameborder="0" scrolling="no" name="' + box_id + '" id="' + box_id + '" src="' + iframe_url.replace('"', "'") + '" width="600px" height="200px"></iframe>';
+}
 
-    if (document.getElementById(div_id) == null) {
+function show_msg(overlay, msg) 
+{
+    overlay.innerHTML = '<div id="' + box_id + '"><div id="' + msg_id + '">' + msg + '</div></div>';
+}
+
+
+/* Create dialog and listen for messages from iframe */
+(function(){
+
+    if (document.getElementById(overlay_id) == null) {
+        // reference our CSS styling
         var head = document.getElementsByTagName("head")[0];
         var css = document.createElement('link');
         css.type = 'text/css';
         css.rel = 'stylesheet';
-        css.href = css_url;
+        css.href = host + path_to_gritttt + "/dialog.css";
         css.media = 'screen';
         head.appendChild(css);
 
-        // display form in iframe
-        var div = document.createElement("div");
-        div.id = div_id;
-        div.title = "click to close window.";
-        div.onclick = function(){
-            document.getElementById(div_id).style.display='none';
+        // display form in iframe that sits on an overlay
+        var overlay = document.createElement("div");
+        overlay.id = overlay_id;
+        overlay.title = "click to close window.";
+        overlay.onclick = function(){
+            document.getElementById(overlay_id).style.display='none';
         };
-        div.innerHTML = '<iframe frameborder="0" scrolling="no" name="' + iframe_id + '" id="' + iframe_id + '" src="' + iframe_url.replace('"', "'") + '" width="600px" height="200px"></iframe>';
-        document.body.insertBefore(div, document.body.firstChild);
-   
-        // listen for success/failure report 
+        document.body.insertBefore(overlay, document.body.firstChild);
+ 
+        // listen for messages 
         // first create IE + others compatible event handler
         var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
         var eventer = window[eventMethod];
@@ -37,21 +53,25 @@
             console.log('parent received message: "' + e.data + '" from ' + e.origin);
             if (e.origin == host) {
                 console.log('host confirmed');
-                if (e.data == 'shared') {
-                    document.getElementById(div_id).style.display='none';
-                    // TODO: refill iframe or nullify div, or make sense in another way of 
-                    //       a follow-up click on bookmarklet
+                if (e.data == 'success') {
+                    //show_share_form(div); // for eventual follow-up click ...
+                    show_msg(overlay, 'Page successfully shared!');
                 }
-                else if(e.data == 'failure') {
-                    alert('oops');
+                else if(e.data == 'login') {
+                    show_msg(overlay, 'Please log in to <a href="' + ttrss_host + '">your tt-rss reader</a>.');
+                }
+                else if(e.data.substring(0,7) == 'failure') {
+                    alert('oops:' + e.data.substring(8));
                 }
             }
         },false);
-
     } else {
-        document.getElementById(div_id).style.display = 'block';
+        document.getElementById(overlay_id).style.display = 'block';
     }
 
-  
+    // create the gritttt-box with the iframe if first click on this page or if we now show a message/
+    if (document.getElementById(box_id) == null || document.getElementById(msg_id) !== null) {       
+        show_share_form(document.getElementById(overlay_id));
+    }
 })()
 
--- shareintt-widget/share.php
+++ shareintt-widget/share.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /* 
  This script takes website details from the form and inserts
  them into the tt-rss DB.
@@ -9,13 +9,55 @@
    also set updated, published & read. Remember new-id
  * Make new entry in ttrss_user_entries, set (new-id->ref_id, gritttt-id->feed_id, user-id->owner_uid) 
  
- TODO: check functions.php in tt-rss for usable code
+ TODO:
+    * Login and possible logout can be done with the JSON API (http://tt-rss.org/redmine/projects/tt-rss/wiki/JsonApiReference). To start with, we could simply check if the user is logged in and report an error if (s)he is not.
+    * The API does not support insertion of a new article (why should it?). Therefore, this needs to be coded by hand
+      Search for 'INSERT' in functions.php to get a start. All we need is to get a database connection and make that insert happen.
  */
 
 
+// This below might be useful to allow sharing only when user is logged in
+//  Then we might need the sessionid from tt-rss here.
+/*
+
+$ttrssServer = 'http://www.nicolashoening.de/tt-rss-test';
+
+function postAPI($data) {                                                                   
+    $data_string = json_encode($data);                                                                                   
+    $ch = curl_init(ttrssServer.'/api');                                                                      
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($data_string))                                                                       
+    );  
+    return curl_exec($ch);
+}
+$data = array("op" => "isLoggedIn"); 
+echo(postAPI($data));
+*/
+
+$path_to_ttrss = '../tt-rss-test/';
+require_once($path_to_ttrss."backend.php");
+require_once($path_to_ttrss."functions.php");
+require_once($path_to_ttrss."fb-prefs.php");
+
+$link = db_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+init_connection($link);
+
+$login = $_POST["login"]; // make empty?
+$password = $_POST["password"];
+
+if (authenticate_user($link, $login, $password)) {
+    db_query($link, "UPDATE ttrss_users SET last_login = NOW() WHERE id = ");
+} else {
+    echo('please log in');
+}
 ?>
+
 <script>
 window.onload = function(){
-        window.parent.postMessage('shared', '*'); //TODO: only send to specific URI
+        window.parent.postMessage('<?php echo $RES;?>', '*'); //TODO: only send to specific URI
 };
 </script>
