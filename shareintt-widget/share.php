d92467bc3111c44d969fe3dd6193609a8ca1e0f2
Nicolas Honing <nhoening@gmail.com>
2012-03-09 01:27:06 +0100

    add seed to guid

--- shareintt-widget/share.php
+++ shareintt-widget/share.php
@@ -5,7 +5,7 @@
 */
 
 // adapt these
-$feed_id = 174;
+$feed_id = 178;
 $o_id = 1;  //admin is 1
 
 
@@ -21,24 +21,20 @@ $MSG = 'success';
 
 if ($_SESSION["uid"] && validate_session($link)) {
     try{
-        /*
-        Update db. We needs to know database credentials, id of user in DB and gritttt-id
-        In particular,
-        - Make new entry in ttrss_entries, set (title, link, content) from request, Remember new-id 
-        - Make new entry in ttrss_user_entries, set (new-id->ref_id, gritttt-id->feed_id, user-id->owner_uid) 
-          also set updated, published & read. 
-        */
         $t = $_POST['gritttt-title'];
         $u = $_POST['gritttt-url'];
+        $u .= ',imported:'.time();
         $c = $_POST['gritttt-comment'];
+        // Make new entry in ttrss_entries, set (title, link, content) from request, Remember new-id 
         db_query($link, "INSERT into ttrss_entries (title, link, guid, updated) VALUES ('$t', '$u', '$u', NOW());");
         if (1 ==1) { // currently only support for MySQL, no postgres love :(
             $last_id = mysql_insert_id();
         } else {
 
         }
+        // Make new entry in ttrss_user_entries
         db_query($link, "INSERT into ttrss_user_entries (ref_id, feed_id, owner_uid, note, published, unread) VALUES ($last_id, $feed_id, $o_id, '$c', 1, 0);");
-    } catch (Exception $e) {  // does not catch DB errors yet?
+    } catch (Exception $e) { // does not catch DB errors yet ?
         $MSG = 'failure:'.$e->getMessage();
     }
 } else {
