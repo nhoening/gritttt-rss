430cd31bd39a4c9cd6e4059ca3406e08016cfa25
Nicolas Honing <nhoening@gmail.com>
2012-03-12 09:40:37 +0100

    use a dummy URL for feed so that it becomes editable

--- create-gritttt-feed.sql
+++ create-gritttt-feed.sql
@@ -1,3 +1,4 @@
 -- Execute this on your tt-rss database to add the gritttt feed entry
 -- This assumes that you use the admin user who has ID:1
-INSERT into ttrss_feeds (title, owner_uid, update_interval) VALUES ('gritttt-feed', 1, -1);
+INSERT into ttrss_feeds (title, owner_uid, update_interval, feed_url, include_in_digest) 
+VALUES ('gritttt-feed', 1, -1, 'http://www.example.com', 0);
